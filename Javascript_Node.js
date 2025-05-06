c2ba1051808222c93c41f0bed9cebf6b6a461fa5
hossenrabiul <robiulhossen0081@gmail.com>
2025-05-06 09:55:42 +0600

    changed

--- Javascript_Node.js
+++
@@ -1,4 +0,0 @@
-// Premitive -  Intiger, string, Boolean, etc
-// not premitive - Object, array etc
-
-// amra jodi kono premitive value ke 1ta funtion er modde pass kori, tarpor value ta jodi sei funtion er vitor change kori thole seta change hve na, but jodi not premitive pass kori,tarpor change kori thole change hbe. karon amra jkn akta premitive value pass kori 1ta funtion ee tkn sei funtion ee value tar 1ta just copy create hy but non premitive er smy referrence hiseve value ta fun e jai.
